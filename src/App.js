b66f228e560567eef49ec1b6c631c7ce0727369b
danielmaytahuari <amaytahuari@gmail.com>
2026-01-02 11:09:32 -0500

    agregar handler a botón Guardar usuario

--- src/App.js
+++ src/App.js
@@ -2251,7 +2251,7 @@ const handleChangePassword = () => {
       
       <div className="flex gap-2">
         <button
-          onClick={}
+          onClick={handleSaveUserEdit}
           className="flex-1 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
         >
           Guardar
