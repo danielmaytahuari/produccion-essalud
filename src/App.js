b46f773005d26db562e55318f945113556c845fb
danielmaytahuari <amaytahuari@gmail.com>
2026-01-13 21:32:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3672,9 +3672,16 @@ const handleChangePassword = () => {
           className="w-full px-4 py-3 bg-green-600 text-white rounded-lg hover:bg-green-700 transition font-semibold"
         >
           Registrar Producción
+       <button
+          onClick={handleSubmit}
+          className="w-full px-4 py-3 bg-green-600 text-white rounded-lg hover:bg-green-700 transition font-semibold"
+        >
+          Registrar Producción
         </button>
       </div>
     );
-   }
+  }
+
+}
 
 export default App;
